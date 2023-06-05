0d970e971e493edec4542d9e44a659e5f0749e88
Lucy <lucybellott@gmail.com>
2023-06-05 17:28:43 -0400

    sets

--- hello.py
+++ hello.py
@@ -70,6 +70,12 @@ del(inventory["ProductNo1_releaseYear"])
 
 print(inventory)
 
-#test commit
-print('test commit')
+#SETS
+
+# Convert list to set
+
+album_list = [ "Michael Jackson", "Thriller", 1982, "00:42:19", \
+              "Pop, Rock, R&B", 46.0, 65, "30-Nov-82", None, 10.0]
+album_set = set(album_list)             
+print(album_set)
 
