3a580477d07aad31ea715e5ad5ef7b8331bf5263
Lucy <lucybellott@gmail.com>
2023-06-12 15:44:23 -0400

    loops

--- hello.py
+++ hello.py
@@ -108,3 +108,12 @@ N = len(dates)
 
 for i in range(N):
     print(dates[i])
+
+ # Use for loop to change the elements in list
+
+squares = ['red', 'yellow', 'green', 'purple', 'blue']
+
+for i in range(0, 5):
+    print("Before square ", i, 'is',  squares[i])
+    squares[i] = 'white'
+    print("After square ", i, 'is',  squares[i])   
