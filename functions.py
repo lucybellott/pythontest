b2b93dc1508f9e4b619ecbf03641a2685da00b70
Lucy <lucybellott@gmail.com>
2023-06-18 20:13:28 -0400

    hackerrank

--- functions.py
+++ functions.py
@@ -63,3 +63,19 @@ pair_sum = 9
   
 # Calling function
 twoSumHashing(num_arr, pair_sum)
+
+def reachNumber(self, target):
+        """
+        :type target: int
+        :rtype: int
+        """
+        target = abs(target)
+        if not target: return 0
+        step = 1
+        total = 0
+        while True:
+            total += step            
+            if total >= target and (total-target)%2 == 0:
+                return step
+            step += 1
+        return -1
